2178ced34dd6e1321e167a0ad2c653eba5325053
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-04 17:41:08 +0600

    StaticMethod vs classMethod

---
+++ inheritance.py
@@ -0,0 +1,7 @@
+# How to access the class name in a method : self.__class__.__name__
+# super() constructor of a class has the access of his parent class attribute/property
+#If you need to call the paren class's magicMethods __init__, you have call like this: super().__init__(parent-class
+# attribute/property)
+
+
+
