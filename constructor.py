2085207f16eeca13fb401bb81cfef7b7bb7a77ec
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-04 12:36:52 +0600

    Calculate discount

--- constructor.py
+++ constructor.py
@@ -2,6 +2,8 @@
 #class theke create howya instance/object self argument er moddhye available hoy.
 
 class Item:
+    # class attributeses
+    payRate=.8 # cal 20% discount
     def __init__(self,name: str,price: float=100,quantity: int=0):
         #Run validation for constructor recieved arguments
         assert len(name)>=3, f"Product name need at last 3 letter!"
@@ -14,5 +16,11 @@ class Item:
         print(f"{name}, created in constructor method")
     def calTotalPrice(self):
         return self.quantity*self.price
+    def calDiscount(self):
+        self.price=self.price*Item.payRate
 phone=Item("Phone",300,5)
 laptop=Item("Laptop",500,3)
+# __dict__ magic attriutes use korle class attribute insatance level e vailable hoy na
+phone.calTotalPrice()
+phone.calDiscount()
+# print(phone.price) //after 20% discount 240
